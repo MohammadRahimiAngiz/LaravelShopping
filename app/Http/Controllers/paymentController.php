e280ee59fdeca6eceeed4a21d905561b7640d8f7
RahimiAngiz <rahimiangiz@chmail.ir>
2021-07-10 08:28:25 +0430

    Rewrite the payment gateway code in cart using the shetabit/payment package

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 
+use App\Payment;
 use App\Services\Cart\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Payment\Facade\Payment as ShetabitPayment;
+use Shetabit\Multipay\Exceptions\InvalidPaymentException;
 
 class paymentController extends Controller
 {
@@ -22,60 +26,50 @@ class paymentController extends Controller
             $order = auth()->user()->orders()->create([
                 'status' => 'unpaid',
                 'price' => $price,
-
             ]);
             $order->products()->attach($orderItems);
-            $token = config('services.payPing.token');
-            $resNumber = Str::random(12);
-            $args = [
-                "amount" => 1000,
-//                "payerIdentity" => "شناسه کاربر در صورت وجود",
-                "payerName" => auth()->user()->name,
-//                "description" => "توضیحات",
-                "returnUrl" => route('payment.callback'),
-                "clientRefId" => $resNumber
-            ];
-
-            $payment = new \PayPing\Payment($token);
-
-            try {
-                $payment->pay($args);
-            } catch (\Exception $e) {
-//                var_dump($e->getMessage());
-                throw $e;
-            }
-//echo $payment->getPayUrl();
-
-//            header('Location: ' . $payment->getPayUrl());
-//            dd($payment->getPayUrl());
-            $order->payments()->create([
-                'resNumber'=>$resNumber,
-                'price'=>$price
-            ]);
-            Cart::flush();
-            return redirect($payment->getPayUrl());
+            // Create new invoice.
+            //todo $invoice = (new Invoice)->amount($price);
+            $invoice = (new Invoice)->amount(1000);
+            // Purchase and pay the given invoice.
+            // You should use return statement to redirect user to the bank page.
+            return ShetabitPayment::callbackUrl(route('payment.callback'))->purchase($invoice, function ($driver, $transactionId) use ($order, $invoice) {
+                // Store transactionId in database as we need it to verify payment in the future.
+                $order->payments()->create([
+                    'resNumber' => $invoice->getUuid(),
+                ]);
+                Cart::flush();
+            })->pay()->render();
         }
         return back();
     }
 
     public function callback(Request $request)
     {
-        $token = config('services.payPing.token');
-
-        $payment = new \PayPing\Payment($token);
-
+        // You need to verify the payment to ensure the invoice has been paid successfully.
+        ////// We use transaction id to verify payments
+        // It is a good practice to add invoice amount as well.
         try {
-            if($payment->verify($request->refid, 1000)){
-                echo "success";
-            }else{
-                echo "fail";
-            }
+            $payment = Payment::with('order')->where('resNumber', $request->clientrefid)->firstOrFail();
+            //todo $receipt = ShetabitPayment::amount($payment->order->price)->transactionId($request->clientrefid)->verify();
+            $receipt = ShetabitPayment::amount(1000)->transactionId($request->clientrefid)->verify();
+            $payment->update([
+                'status' => 1,
+            ]);
+            $payment->order()->update([
+                'status' => 'paid',
+            ]);
+            alert()->success('Successful');
+            return redirect('/products');
+        } catch (InvalidPaymentException $exception) {
+            /**
+             * when payment is not verified, it will throw an exception.
+             * We can catch the exception to handle invalid payments.
+             * getMessage method, returns a suitable message that can be used in user interface.
+             **/
+//            return $exception->getMessage();
+            alert()->error($exception->getMessage());
+            return redirect('/products');
         }
-        catch (PayPingException $e) {
-            foreach (json_decode($e->getMessage(), true) as $msg) {
-                echo $msg;
-            }
-        }
-//        return $request->all();
     }
 }
